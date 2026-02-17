daaa26052b5d7615088ca7d9d166ba3e8890cbca
aashi-s <shah.aashi28@gmail.com>
2026-02-17 17:05:12 -0500

    made it functional again

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import Constants from "expo-constants";
 import * as Notifications from "expo-notifications";
-import React, { useEffect, useRef, useState } from "react";
+import * as TaskManager from "expo-task-manager";
+import React, { useEffect, useState } from "react";
 import {
   Alert,
   Button,
@@ -28,9 +29,12 @@ Notifications.setNotificationHandler({
     shouldSetBadge: false,
   }),
 });
+
+// TYPES
 type ApprovalType = "pending" | "approved" | "declined";
 type ApplicationType = "pending" | "success" | "error" | "null";
 function HomeScreen() {
+  // STATES
   const [expoPushToken, setExpoPushToken] = useState("");
   const [sessionDetails, setSessionDetails] = useState<{
     userInputs?: string;
@@ -57,39 +61,30 @@ function HomeScreen() {
   const [notification, setNotification] = useState<
     Notifications.Notification | undefined
   >(undefined);
-  const [sessionId, setSessionId] = useState(""); // does it need to be kept in state
-  const descriptionRef = useRef("");
+  const [sessionId, setSessionId] = useState("");
+  const [isConnected, setIsConnected] = useState(false); //Is a device connected?
+  const [isAnalyzing, setIsAnalyzing] = useState(false); //Is the analysis process ongoing?
+  const [treatment, setTreatment] = useState<string>(""); //Treatment value returned
+  const [infestation, setInfestation] = useState<boolean | undefined>(
+    undefined
+  ); //Infestation boolean value returned
+  const [connectedDevice, setConnectedDevice] = useState<BLEDevice>(); //What device is connected?
+  const [temperature, setTemperature] = useState("");
+  const [treatmentOnMicrocontroller, setTreatmentOnMicrocontroller] = useState<
+    string | null
+  >(null); // treatment value that microcontroller has
+  const [encodedImage, setEncodedImage] = useState("");
 
-  const backendUrl = "https://loriann-imbricative-transfixedly.ngrok-free.dev";
-  const BLTManager = new BleManager();
-
-  // update these
+  // CONSTANTS
+  const BACKEND_URL = "https://loriann-imbricative-transfixedly.ngrok-free.dev";
   const SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
   const TEMPERATURE_UUID = "6d68efe5-04b6-4a85-abc4-c2670b7bf7fd";
   const TREATMENT_UUID = "f27b53ad-c63d-49a0-8c0f-9f297e6cc520";
+  const BACKGROUND_NOTIFICATION_TASK = "BACKGROUND-NOTIFICATION-TASK";
+  // const CONSOLE_UUID = "";
+  const THREE_MONTHS = 3 * 30 * 24 * 60 * 60; // in seconds
 
-  //Is a device connected?
-  const [isConnected, setIsConnected] = useState(false);
-
-  //Is the analysis process ongoing?
-  const [isAnalyzing, setIsAnalyzing] = useState(false);
-
-  //Treatment value returned
-  const [treatment, setTreatment] = useState<string>("");
-
-  //Infestation boolean value returned
-  const [infestation, setInfestation] = useState<boolean | undefined>(
-    undefined
-  );
-
-  //What device is connected?
-  const [connectedDevice, setConnectedDevice] = useState<BLEDevice>();
-
-  const [temperature, setTemperature] = useState("");
-  // const [treatment, setTreatment] = useState(false);
-  const [treatmentOnMicrocontroller, setTreatmentOnMicrocontroller] = useState<
-    string | null
-  >(null);
+  const BLTManager = new BleManager();
 
   async function registerForPushNotificationsAsync() {
     let token;
@@ -152,33 +147,32 @@ function HomeScreen() {
       Notifications.addNotificationResponseReceivedListener((response) => {
         console.log(response);
       });
+    registerBackgroundNotificationTask();
     return () => {
       notificationListener.remove();
       responseListener.remove();
     };
   }, []);
 
-  async function schedulePushNotification(seconds?: number) {
-    // update this to be 3 months from now
+  async function schedulePushNotification(
+    title: string,
+    body: string,
+    seconds?: number
+  ) {
     const content = {
-      title: "Test notification",
-      body: "123",
-      data: { data: "data", test: { test1: "more data" } },
+      title,
+      body,
     };
-    const delay = 3 * 30 * 24 * 60 * 60;
     const sendDate = new Date();
-    sendDate.setSeconds(sendDate.getSeconds() + delay);
+    sendDate.setSeconds(sendDate.getSeconds() + (seconds || 30));
 
     await Notifications.scheduleNotificationAsync({
       content,
       trigger: {
         type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
         seconds: seconds || 30,
-        // seconds: delay,
       },
-    }).then((res) => {
-      console.log("notification scheduled response: ", res); // 75eb45cc-ed67-4536-b270-d0c1eadbebd4
-      const notificationId = uuid.v4();
+    }).then((notificationId) => {
       const notificationDetails = {
         sessionId: sessionId,
         status: "scheduled",
@@ -193,30 +187,17 @@ function HomeScreen() {
     });
   }
 
-  // doesnt seem to be used
-  // useEffect(() => {
-  //   if (!connectedDevice || !connectedDevice.isConnected) {
-  //     return;
-  //   }
-  //   const sub = connectedDevice.monitorCharacteristicForService(
-  //     SERVICE_UUID,
-  //     TEMPERATURE_UUID,
-  //     (error, characteristic) => {
-  //       if (error || !characteristic?.value) {
-  //         return;
-  //       }
-  //       setTemperature(base64.decode(characteristic?.value));
-  //       console.log(
-  //         "USEEFFECT Message update received: ",
-  //         base64.decode(characteristic?.value)
-  //       );
-  //     }
-  //   );
-  //   return () => sub.remove();
-  // }, [connectedDevice]);
-
   async function openAppNotificationSettings() {
     await Linking.openSettings();
+  }
+
+  TaskManager.defineTask(BACKGROUND_NOTIFICATION_TASK, (data) => {
+    console.log("Notification received in background!", data);
+    return Promise.resolve();
+  });
+
+  async function registerBackgroundNotificationTask() {
+    await Notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK);
   }
 
   // Scans availbale BLT Devices and then call connectDevice
@@ -266,6 +247,7 @@ function HomeScreen() {
       const isDeviceConnected = await connectedDevice.isConnected();
       if (isDeviceConnected) {
         BLTManager.cancelTransaction("temperaturetransaction");
+        // BLTManager.cancelTransaction("consoletransaction");
         BLTManager.cancelTransaction("nightmodetransaction");
 
         BLTManager.cancelDeviceConnection(connectedDevice.id).then(() =>
@@ -334,7 +316,6 @@ function HomeScreen() {
           .readCharacteristicForService(SERVICE_UUID, TREATMENT_UUID)
           .then((valenc) => {
             if (valenc?.value)
-              // setTreatment(StringToBool(base64.decode(valenc?.value)));
               setTreatmentOnMicrocontroller(base64.decode(valenc?.value));
           });
 
@@ -378,11 +359,24 @@ function HomeScreen() {
           "treatmenttransaction"
         );
 
+        // Console
+        // device.monitorCharacteristicForService(
+        //   SERVICE_UUID,
+        //   CONSOLE_UUID,
+        //   (error, characteristic) => {
+        //     if (characteristic?.value != null) {
+        //       console.log(
+        //         "Console update received: ",
+        //         base64.decode(characteristic?.value)
+        //       );
+        //     }
+        //   },
+        //   "consoletransaction"
+        // );
+
         console.log("Connection established");
       });
   }
-
-  const [encodedImage, setEncodedImage] = useState("");
 
   const handleUploadImage = async () => {
     const result = await launchImageLibrary({
@@ -403,7 +397,7 @@ function HomeScreen() {
   const handleStartAnalysis = async (temperature: string) => {
     try {
       setIsAnalyzing(true);
-      const response = await fetch(`${backendUrl}/temperature`, {
+      const response = await fetch(`${BACKEND_URL}/temperature`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -411,7 +405,7 @@ function HomeScreen() {
         body: JSON.stringify({
           temperature: temperature == "" ? "20" : temperature,
           image: encodedImage,
-          overrideTreatment: null, // type in any treatment name string here, or leave it none
+          overrideTreatment: "test treatment", // type in any treatment name string here, or leave it none
         }),
       });
       if (response.ok) {
@@ -427,26 +421,32 @@ function HomeScreen() {
 
         // Now you can work with the actual response data
         console.log(responseData);
+
         const sessionUpdate = {
           ...sessionDetails,
-          miteCount: 100, // responseData["mite_count"],
-          infestation: true, // responseData["infestation"],
-          temperature: "20", // temperature,
-          treatment: "thymol", // responseData["treatment_recommendation"],
+          miteCount: responseData["mite_count"],
+          infestation: responseData["infestation"],
+          temperature: temperature,
+          treatment: responseData["treatment_recommendation"],
           approval: "pending" as ApprovalType,
-          delay: false, //responseData["delay"], // verify that the boolean works here
+          delay: responseData["delay"],
           applied: "pending" as ApplicationType,
         };
+        console.log(sessionUpdate);
         setSessionDetails(sessionUpdate);
         const currSession = uuid.v4();
         setSessionId(currSession);
         userStorage.set(currSession, JSON.stringify(sessionUpdate));
         setTreatment(responseData["treatment_recommendation"]);
-        setInfestation(responseData["infestation"]); // also check if boolean works here
+        setInfestation(responseData["infestation"]);
         // if infestation is false, also schedule a notification 3 months from now
-        // if (!responseData["infestation"]) {
-        //   await schedulePushNotification(7.884 * 10 ** 6); // 3 months from now
-        // }
+        if (!responseData["infestation"]) {
+          await schedulePushNotification(
+            "It's time to check your hive!",
+            "Take a quick picture of your sticky board to start the process",
+            THREE_MONTHS
+          ); // 3 months in seconds
+        }
       } else {
         // Handle error response
         console.error("Error:", response.statusText);
@@ -528,7 +528,7 @@ function HomeScreen() {
           }}
         >
           {temperature == ""
-            ? "Nothing yet"
+            ? "Temperature is not provided"
             : "Current temperature: " + temperature}
         </Text>
       </View>
@@ -539,10 +539,19 @@ function HomeScreen() {
       <View
         style={{
           justifyContent: "space-around",
-          alignItems: "flex-start",
-          flexDirection: "row",
+          alignItems: "center",
+          flexDirection: "column",
         }}
       >
+        <Text
+          style={{
+            fontSize: 15,
+            fontFamily: "Cochin",
+            fontWeight: "900",
+          }}
+        >
+          Infestation level
+        </Text>
         <Text
           style={{
             fontSize: 15,
@@ -552,7 +561,7 @@ function HomeScreen() {
           {infestation == true
             ? "Your hive is infested, see our recommendation"
             : infestation == false
-            ? "Come back in 3 months, we'll send you a notification"
+            ? "Not infested! We'll notify you to check again in 3 months"
             : "Run analysis to see if your hive is infested"}
         </Text>
       </View>
@@ -563,10 +572,19 @@ function HomeScreen() {
       <View
         style={{
           justifyContent: "space-around",
-          alignItems: "flex-start",
-          flexDirection: "row",
+          alignItems: "center",
+          flexDirection: "column",
         }}
       >
+        <Text
+          style={{
+            fontSize: 15,
+            fontFamily: "Cochin",
+            fontWeight: "900",
+          }}
+        >
+          {treatment == "None" ? "No treatment needed!" : "We recommend..."}
+        </Text>
         <Text
           style={{
             fontSize: 15,
@@ -577,150 +595,90 @@ function HomeScreen() {
             ? "Run analysis to get a treatment"
             : treatment == "null"
             ? "The temperature isn't quite right, don't treat the hive for now!"
-            : "We recommend " + treatment}
+            : treatment !== "None" && treatment}
         </Text>
       </View>
 
-      <View style={{ paddingBottom: 20 }}></View>
-
-      {/* Start Analysis Button */}
-      <View
-        style={{
-          justifyContent: "space-around",
-          alignItems: "flex-start",
-          flexDirection: "row",
-        }}
-      >
-        <TouchableOpacity style={{ width: 120 }}>
-          {!isAnalyzing ? (
+      <View style={{ paddingBottom: 20, flexDirection: "row" }}>
+        <View>
+          <TouchableOpacity
+            style={{
+              width: "100%",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "10",
+              flexDirection: "row",
+            }}
+          >
+            {/* Start Analysis Button */}
             <Button
-              title={
-                temperature == "" ? "Needs a temp first" : "Start Analysis"
-              }
+              title={isAnalyzing ? "Analyzing" : "Start Analysis"}
               onPress={() => {
                 handleStartAnalysis(temperature);
               }}
-              // disabled={temperature == "" ? true : false}
+              disabled={isAnalyzing}
             />
-          ) : (
-            <Button title="Analyzing" disabled={false} />
-          )}
-        </TouchableOpacity>
-      </View>
-
-      {/* Upload Image Button */}
-      <View
-        style={{
-          justifyContent: "space-around",
-          alignItems: "flex-start",
-          flexDirection: "row",
-        }}
-      >
-        <TouchableOpacity style={{ width: 120 }}>
-          <Button
-            title="Upload Image"
-            disabled={false}
-            onPress={() => handleUploadImage()}
-          />
-          <Button
-            title="Schedule Notification"
-            onPress={async () => {
-              await schedulePushNotification();
-            }}
-          />
-          <Button
-            title={
-              sessionDetails.approval == "pending"
-                ? "Approve recommended treatment"
-                : "Treatment approved...sent to pump"
-            }
-            onPress={() => {
-              const sessionUpdate = {
-                ...sessionDetails,
-                approval: "approved" as ApprovalType,
-              };
-              setSessionDetails(sessionUpdate);
-              userStorage.set(sessionId, JSON.stringify(sessionUpdate));
-              if (treatment != "null") {
-                sendTreatment(treatment);
+            {/* Upload Image Button */}
+            <Button
+              title="Upload Image"
+              disabled={false}
+              onPress={() => handleUploadImage()}
+            />
+            <Button
+              title="Schedule Notification"
+              onPress={async () => {
+                await schedulePushNotification("test for now", "30s from now");
+              }}
+            />
+            <Button
+              title={
+                sessionDetails.approval == "pending"
+                  ? "Approve recommended treatment"
+                  : sessionDetails.approval == "approved"
+                  ? "Treatment approved...sent to pump"
+                  : "Declined recommended treatment"
               }
-            }}
-            disabled={
-              sessionDetails.approval != "pending" || treatment == "null"
-            }
-          />
-          <Button
-            title="Get last notification details"
-            onPress={() => {
-              const notifIds = notificationStorage.getAllKeys();
-              const data =
-                notifIds.length > 0
-                  ? notificationStorage.getString(notifIds[0]) || ""
-                  : "";
-              console.log("notif details from database", JSON.parse(data));
-            }}
-            disabled={false}
-          />
-        </TouchableOpacity>
+              onPress={() => {
+                const sessionUpdate = {
+                  ...sessionDetails,
+                  approval: "approved" as ApprovalType,
+                };
+                setSessionDetails(sessionUpdate);
+                userStorage.set(sessionId, JSON.stringify(sessionUpdate));
+                if (treatment != "null") {
+                  sendTreatment(treatment);
+                }
+              }}
+              disabled={
+                sessionDetails.approval != "pending" || treatment == "null"
+              }
+            />
+            <Button
+              title="Get last notification details"
+              onPress={() => {
+                const notifIds = notificationStorage.getAllKeys();
+                const data =
+                  notifIds.length > 0
+                    ? notificationStorage.getString(notifIds[0]) || ""
+                    : "";
+                console.log("notif details from database", JSON.parse(data));
+              }}
+            />
+            <Button
+              title="Get full user database"
+              onPress={() => {
+                const userIds = userStorage.getAllKeys();
+                console.log(userIds);
+                for (const id of userIds) {
+                  const res = userStorage.getString(id);
+                  console.log("user info", id, JSON.parse(res || ""));
+                }
+              }}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
-
-      {/* <View style={{ height: Dimensions.get("screen").height - 132 }}>
-        <Text
-          style={{
-            fontSize: 18,
-            margin: 16,
-            fontWeight: "700",
-          }}
-        >
-          TASK LIST
-        </Text>
-        <TextInput
-          placeholder="Enter New Task"
-          autoCapitalize="none"
-          nativeID="description"
-          multiline={true}
-          numberOfLines={8}
-          value={descriptionRef.current}
-          onChangeText={(text) => {
-            descriptionRef.current = text;
-          }}
-          style={{
-            fontSize: 20,
-            borderWidth: 1,
-            borderRadius: 4,
-            // borderColor: "#455fff",
-            paddingHorizontal: 8,
-            paddingVertical: 4,
-            marginBottom: 0,
-            marginHorizontal: 16,
-          }}
-        />
-        <TouchableOpacity
-          style={{
-            backgroundColor: "grey",
-            padding: 10,
-            borderRadius: 5,
-            marginTop: 8,
-            marginLeft: 16,
-            width: 120,
-          }}
-          onPress={() => {
-            createNewTask();
-          }}
-        >
-          <Text
-            style={{
-              color: "white",
-              textAlign: "center",
-              fontWeight: "600",
-              fontSize: 12,
-            }}
-          >
-            SAVE TASK
-          </Text>
-        </TouchableOpacity>
-        <Text>{JSON.stringify(tasks, null, 2)}</Text>
-      </View> */}
     </View>
   );
 }
